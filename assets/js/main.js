6a1efc0a313c55648690b2fc2702492641c4cc3d
vedhalam-vik <153629693+vedhalam-vik@users.noreply.github.com>
2026-02-03 10:56:44 +0530

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".reveal").forEach(el=>{
-  window.addEventListener("scroll",()=>{
-    if(el.getBoundingClientRect().top < window.innerHeight - 80){
-      el.classList.add("active");
-    }
-  });
-});
